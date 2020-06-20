f2876a7d8cda91b8530da1228d2de9a018641388
roshan129 <roshanadke2015>
2020-06-20 12:57:59 +0530

    commit

--- app/src/main/java/com/adivid/workmanagersimplifiedcoding/MainActivity.java
+++ app/src/main/java/com/adivid/workmanagersimplifiedcoding/MainActivity.java
@@ -9,7 +9,10 @@ import androidx.work.OneTimeWorkRequest;
 import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -23,6 +26,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("my_shared_pref", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        int i = sharedPreferences.getInt("value", 999);
+        if (i == 999) {
+            editor.putInt("value", 1);
+            editor.commit();
+        }
+
+        Log.d("tag", "value main act: "+ i);
 
         final Data data = new Data.Builder()
                 .putString(KEY_TASK_DEC, "Hey I am sending the work data")
@@ -41,21 +55,21 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                WorkManager.getInstance().enqueue(request);
+                WorkManager.getInstance(MainActivity.this).enqueue(request);
 
             }
         });
 
         final TextView textView = findViewById(R.id.textView);
 
-        WorkManager.getInstance().getWorkInfoByIdLiveData(request.getId())
+        WorkManager.getInstance(MainActivity.this).getWorkInfoByIdLiveData(request.getId())
                 .observe(this, new Observer<WorkInfo>() {
                     @Override
                     public void onChanged(WorkInfo workInfo) {
 
-                        if(workInfo != null){
+                        if (workInfo != null) {
 
-                            if(workInfo.getState().isFinished()){
+                            if (workInfo.getState().isFinished()) {
                                 Data data1 = workInfo.getOutputData();
                                 String output = data1.getString(MyWorker.KEY_TASK_OUTPUT);
                                 textView.append(output + "\n");
--- app/src/main/java/com/adivid/workmanagersimplifiedcoding/MyWorker.java
+++ app/src/main/java/com/adivid/workmanagersimplifiedcoding/MyWorker.java
@@ -4,7 +4,9 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -14,15 +16,30 @@ import androidx.work.WorkerParameters;
 
 public class MyWorker extends Worker {
 
+    private static final String TAG = "MyWorker";
     public static final String KEY_TASK_OUTPUT = "key_task_output";
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
 
     public MyWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
+
+        sharedPreferences = context.getSharedPreferences("my_shared_pref", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
     }
 
     @NonNull
     @Override
     public Result doWork() {
+        int i= sharedPreferences.getInt("value", 999);
+        i++;
+        editor.putInt("value", i);
+        editor.commit();
+        Log.d(TAG, "value i : " + i);
+
+
         Data data = getInputData();
         String desc = data.getString(MainActivity.KEY_TASK_DEC);
         displayNotification("This is title", desc);
@@ -34,12 +51,12 @@ public class MyWorker extends Worker {
         return Result.success(data1);
     }
 
-    private void displayNotification(String task, String desc){
+    private void displayNotification(String task, String desc) {
 
         NotificationManager manager =
                 (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
 
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel channel = new NotificationChannel("simplifiedcoding", "simplifiedcoding",
                     NotificationManager.IMPORTANCE_DEFAULT);
             manager.createNotificationChannel(channel);
@@ -49,9 +66,11 @@ public class MyWorker extends Worker {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "simplifiedcoding")
                 .setContentTitle(task)
                 .setContentText(desc)
-                .setSmallIcon(R.drawable.ic_launcher_foreground);
+                .setSmallIcon(R.mipmap.ic_launcher);
 
         manager.notify(1, builder.build());
 
     }
+
+
 }
